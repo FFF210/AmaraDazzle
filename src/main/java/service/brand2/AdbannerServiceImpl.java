333899f839c1c83fc08f29b10f8a2e4fa9fa5f02
Kim, Sojin <xozinee@gmail.com>
2025-09-30 12:03:26 +0900

    [feat] bannerForm 관련 수정

--- src/main/java/service/brand2/AdbannerServiceImpl.java
+++ src/main/java/service/brand2/AdbannerServiceImpl.java
@@ -6,59 +6,38 @@ import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 
-import dao.UploadFileDAO;
-import dao.brand2.AdminPaymentDAO;
 import dao.brand2.BannerDAO;
 import dao.brand2.BannerDAOImpl;
-import dto.AdminPayment;
 import dto.Banner;
-import dto.UploadFile;
 import dto.brand2.BannerForm;
 import util.MybatisSqlSessionFactory;
 
 public class AdbannerServiceImpl implements AdbannerService {
 
 	private BannerDAO bannerDAO;
+//	private AdminPaymentDAO adminPaymentDAO;
 
 	public AdbannerServiceImpl() {
+		// dto 객체 초기화
 		bannerDAO = new BannerDAOImpl();
+//		this.adminPaymentDAO = new AdminPaymentDAOImpl();
 	}
 
-	
-	// // 배너 신청
+//	배너 신청
 	@Override
 	public void registerBanner(BannerForm bannerForm) throws Exception {
 
 		try (SqlSession sqlSession = MybatisSqlSessionFactory.getSqlSessionFactory().openSession()) {
-
-			// MyBatis에서 Mapper 구현체 생성
-			bannerDAO = sqlSession.getMapper(BannerDAO.class);
-//			UploadFileDAO uploadFileDAO = sqlSession.getMapper(UploadFileDAO.class);
-			AdminPaymentDAO adminPaymentDAO = sqlSession.getMapper(AdminPaymentDAO.class);
-
-			// 1. 업로드 파일 DTO 변환 + 저장
-//	  UploadFile file = new UploadFile();
-//	  file.setUploadFileId(bannerForm.getUploadFileId());
-//	  uploadFileDAO.insertUploadFile(file);
+//            AdminPaymentDAO adminPaymentDAO = sqlSession.getMapper(AdminPaymentDAO.class);
 
 			// 2. 배너 DTO 변환 + 저장
-			Banner banner = new Banner();
-			banner.setBrandId(bannerForm.getBrandId());
-			banner.setManagerName(bannerForm.getManagerName());
-			banner.setManagerTel(bannerForm.getManagerTel());
-			banner.setStartDate(bannerForm.getStartDate());
-			banner.setEndDate(bannerForm.getEndDate());
-			banner.setBannerName(bannerForm.getBannerName());
-			banner.setBannerMessage(bannerForm.getBannerMessage());
-			banner.setBrandUrl(bannerForm.getBrandUrl());
-			banner.setStatus("PENDING");
+			Banner banner = Banner.from(bannerForm);
+			bannerDAO.insertBannerForm(banner);
 
-			bannerDAO.insertBanner(banner);
-
-			// 3. 결제 DTO 변환 + 저장
-			AdminPayment payment = new AdminPayment();
-			payment.setBrandId(bannerForm.getBrandId());
-			adminPaymentDAO.insertAdminPayment(payment);
+//			// 3. 결제 DTO 변환 + 저장
+//			AdminPayment payment = new AdminPayment();
+//			payment.setBrandId(bannerForm.getBrandId());
+//			adminPaymentDAO.insertAdminPayment(payment);
 
 			sqlSession.commit();
 		} catch (Exception e) {
