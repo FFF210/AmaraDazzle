d6a8fcc666aa7d9941d1ca127b055aa871576758
kyeongeunx <gyeongeh@gmail.com>
2025-10-05 18:44:49 +0900

    [fix] brand2 이벤트 필터링 추가

--- src/main/java/dao/brand2/EventDAO.java
+++ src/main/java/dao/brand2/EventDAO.java
@@ -8,4 +8,7 @@ import dto.brand2.EventList;
 public interface EventDAO {
 	// 이벤트 목록 조회
 	List<EventList> selectEventList(Map<String, Object> params);
+
+	// 이벤트 개수 조회
+	public Integer selectEventCount(Map<String, Object> params);
 }
